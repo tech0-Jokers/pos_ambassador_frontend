d5f41b91b790b923364d614aa4cbbf3000f858f2
suzuki <suzuyu33z@gmail.com>
2024-11-13 23:48:48 +0900

    no message

--- src/components/RegistrationApp.tsx
+++ src/components/RegistrationApp.tsx
@@ -73,7 +73,7 @@ export default function RegistrationApp() {
 
       // FastAPIへPOSTリクエストを送信
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/recieving_register`,
+        `${process.env.NEXT_PUBLIC_API_URL}/receiving_register`,
         {
           method: "POST",
           headers: {
